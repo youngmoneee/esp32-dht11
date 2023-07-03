4ee2d695e3671e390289e7f4dab43961f3c58a03
youngmon <79129960+youngmoneee@users.noreply.github.com>
2023-07-03 18:18:57 +0900

    Create sensor-serial.cpp
    센서 데이터 읽어서 Serial 모니터로 표현

---
+++ sensor-serial.cpp
@@ -0,0 +1,32 @@
+#include "DHT.h"
+
+#define DHTPIN D4     // 센서를 연결한 디지털 핀 번호
+#define DHTTYPE DHT11   // DHT 11 
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: "); 
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: "); 
+  Serial.print(t);
+  Serial.println(" *C");
+}
